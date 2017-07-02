c9066c698a476240a07ada7853d7c788bbfa1985
Roman Veselovsky <romanveselovsky@gmail.com>
2017-07-02 20:18:24 +0300

    Task4: js function pow(x,n)

--- work/main.js
+++ work/main.js
@@ -1,3 +1,14 @@
 window.onload = function() {
     console.log('init js');
 };
+
+function pow(x, n) {
+    var result;
+    var x;
+    var n;
+    result = Math.pow(x, n);
+
+    return result;
+}
+
+console.log( pow(2, 5) );
