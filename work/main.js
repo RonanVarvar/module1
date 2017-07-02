dee50ffa9dad1a2a639827c298f8cf86ffdd9378
Roman Veselovsky <romanveselovsky@gmail.com>
2017-07-02 21:21:15 +0300

    Task13: concat

--- work/main.js
+++ work/main.js
@@ -77,3 +77,13 @@ function converttoString() {
 }
 
 converttoString();
+
+function combine() {
+    var arr1 = ['a', 'b', 'c'];
+    var arr2 = [1, 2, 3];
+    var newArr = arr1.concat(arr2);
+
+    console.log(newArr);
+}
+
+combine();
