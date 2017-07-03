7b0c96352f9852de866d5beb08aa7abaca3bcb0e
Roman Veselovsky <romanveselovsky@gmail.com>
2017-07-03 22:24:51 +0300

    Task22: swap input

--- work/main.js
+++ work/main.js
@@ -158,3 +158,16 @@ function addTextInput () {
 
     document.getElementById('text').value = text;
 }
+
+var click = document.getElementById('button2');
+
+click.addEventListener('click', swapInput);
+
+function swapInput () {
+    var input1 = document.getElementById('input1');
+    var input2 = document.getElementById('input2');
+    var input1Value = input1.value;
+    var input2Value = input2.value;
+    input1.value = input2Value;
+    input2.value = input1Value;
+}
