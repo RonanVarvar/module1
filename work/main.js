fbcdbd0816a325ad50e41303f9d6fe418f8f346b
Roman Veselovsky <romanveselovsky@gmail.com>
2017-07-03 23:54:36 +0300

    Task20: events mapping

--- work/main.js
+++ work/main.js
@@ -202,3 +202,35 @@ inputBlur.onblur = function() {
 
     console.log (valueInput);
 };
+
+window.onload = function() {
+    var elements = {
+        prev: document.getElementById('prev'),
+        current: document.getElementById('current'),
+        next: document.getElementById('next')
+    };
+
+    var events = {
+        prev: function () {
+            console.log('prev');
+        },
+
+        current: function () {
+            console.log('current');
+        },
+
+        next: function () {
+            console.log('next');
+        }
+    };
+
+    var key;
+    var click;
+
+    for (key in events) {
+        for (key in elements) {
+            click = elements[key];
+            click.addEventListener('click', events[key]);
+        }
+    }
+};
